d048fde3c9bb16f130a1843176c2015d0318b34b
Toon De Coninck <toon@auth0.com>
2017-05-31 15:26:29 +0200

    removed unused reference in devtools.jsx

--- client/src/containers/DevTools/devtools.jsx
+++ client/src/containers/DevTools/devtools.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
-import DockMonitor from 'redux-devtools-dock-monitor';
 
 export default createDevTools(
   <LogMonitor theme="tomorrow" />
